a5422ee545ea732bbee584eba5681afb33e0a5c2
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-01-11 21:27:50 +0530

    --- Auto Git Commit ---

--- studentapp.groovy
+++ studentapp.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('build') {
             steps {
-                echo ''
+                sh 'mvn clean package'
             }
         }
         stage('test') {
