ccaf80a3f27ccc0244ba53631515cb10dfd442da
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-01-12 16:39:09 +0530

    --- Auto Git Commit ---

--- studentapp.groovy
+++ studentapp.groovy
@@ -9,6 +9,9 @@ pipeline {
         stage('build') {
             steps {
                 sh 'mvn clean package'
+                sh'rm -rf ~/.m2/repository/org/apache/maven/plugins/maven-war-plugin
+rm -rf ~/.m2/repository/com/thoughtworks/xstream
+
             }
         }
         stage('test') {
