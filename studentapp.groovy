b6a43377c3bc264f363dbfc297c37a76b7f3c817
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-01-16 19:52:13 +0530

    --- Auto Git Commit ---

--- studentapp.groovy
+++ studentapp.groovy
@@ -10,7 +10,7 @@ pipeline {
                 git 'https://github.com/Bhushan-88/studentapp-ui.git'
             }
         }
-*        stage('build') {
+        stage('build') {
             steps {
                 sh '/opt/apache-maven-3.9.1/bin/mvn package'
             }
