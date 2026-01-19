38696b39463d914f0ebcf590249e37da53c9a7c9
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-01-19 15:03:13 +0530

    --- Auto Git Commit ---

--- studentapp.groovy
+++ studentapp.groovy
@@ -23,7 +23,7 @@ pipeline {
             }
         }
 */
-        stage('test') {
+/*        stage('test') {
             steps{
                 sh 'mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
   -Dsonar.projectKey=student-app \
@@ -31,6 +31,8 @@ pipeline {
   -Dsonar.login=sqp_c9d3a245c54ddda139cd7864b873de82c66db991'
             }
         }
+*/
+
         stage('deploy') {
             steps{
                 echo 'deploy is successful'
