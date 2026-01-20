c28c462c584eea582228997ea6ba87abe265ea1b
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-01-20 14:27:36 +0530

    --- Auto Git Commit ---

--- studentapp.groovy
+++ studentapp.groovy
@@ -41,14 +41,6 @@ pipeline {
             }
         }
 
-        stage('Quality Gate') {
-            steps {
-                timeout(time: 5, unit: 'MINUTES') {
-                    waitForQualityGate abortPipeline: true
-                }
-            }
-        }
-
         stage('deploy') {
             steps{
                 echo 'Quality Gate passed. Deploying application...'
