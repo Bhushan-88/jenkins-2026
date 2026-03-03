688c59c264931315ce7099da9b91945916772b22
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-03-03 11:13:21 +0530

    --- Auto Git Commit ---

--- demo.groovy
+++ demo.groovy
@@ -3,5 +3,6 @@ pipeline {
     stages {
         stage('pull') {
             steps {
-                git 'https://github.com/your-repo.git'  
+                git 'https://github.com/your-repo.git' 
+            }
 }
