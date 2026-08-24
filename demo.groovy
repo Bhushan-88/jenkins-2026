aa3f7a2392af418fc228ad6e67016dadbd52bbba
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-24 16:10:15 +0530

    --- Auto Git Commit ---

--- demo.groovy
+++ demo.groovy
@@ -9,31 +9,3 @@ pipeline {
     }
 }
 
-pipeline {
-    agent any;
-    stages{
-        stage("code"){
-            steps{
-                echo "code clone done"
-            }
-        }
-        stage("build"){
-            steps{
-                echo "docker build stage success"
-            }
-            
-        }
-        stage("test"){
-            steps{
-                echo "test tester will give"
-            }
-            
-        }
-        stage("deploy"){
-            stpes{
-                echo "deploy by docker compose"
-            }
-            
-        }
-    }
-}
