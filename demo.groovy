bc9b08e0633c200c7141a9b1a0833129dee9cca3
Bhushan-88 <bhushabdurgawli1@gmail.com>
2026-08-25 22:19:49 +0530

    Update email notifications in pipeline for build success and failure

--- demo.groovy
+++ demo.groovy
@@ -45,6 +45,24 @@ pipeline {
             
         }
     }
+    post{
+        success{
+            script{
+                emailext from: 'bhushandurgawli1@gmail.com',
+                to: 'bhushandurgawli1@gmail.com',
+                body: 'Build success for Demo CICD App',
+                subject: 'Build success for Demo CICD App'
+            }
+        }
+        failure{
+            script{
+                emailext from: 'bhushandurgawli1@gmail.com',
+                to: 'bhushandurgawli1@gmail.com',
+                body: 'Build Failed for Demo CICD App',
+                subject: 'Build Failed for Demo CICD App'
+            }
+        }
+    }
 
 }
 
@@ -99,16 +117,16 @@ pipeline{
 post{
         success{
             script{
-                emailext from: 'mentor@trainwithshubham.com',
-                to: 'mentor@trainwithshubham.com',
+                emailext from: 'bhushandurgawli1@gmail.com',
+                to: 'bhushandurgawli1@gmail.com',
                 body: 'Build success for Demo CICD App',
                 subject: 'Build success for Demo CICD App'
             }
         }
         failure{
             script{
-                emailext from: 'mentor@trainwithshubham.com',
-                to: 'mentor@trainwithshubham.com',
+                emailext from: 'bhushandurgawli1@gmail.com',
+                to: 'bhushandurgawli1@gmail.com',
                 body: 'Build Failed for Demo CICD App',
                 subject: 'Build Failed for Demo CICD App'
             }
